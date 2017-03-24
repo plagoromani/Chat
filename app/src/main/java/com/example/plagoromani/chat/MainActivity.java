2607521d40a4999ba2eef2a74fd91e9f50a756cb
plagoromani <plagoromani@nautilus18.danielcastelao.org>
2017-03-24 12:45:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/plagoromani/chat/MainActivity.java
@@ -0,0 +1,231 @@
+package com.example.plagoromani.chat;
+
+import android.os.Build;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private WebSocketClient mWebSocketClient;
+
+    private static final int MY_PERMISSIONS_REQUEST_INTERNET = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    private void connectWebSocket() {
+
+        URI uri;
+        try {
+            uri = new URI("ws:https://chat-pablite5.c9users.io:8080");
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+
+        Map<String, String> headers = new HashMap<>();
+
+        mWebSocketClient = new WebSocketClient(uri, new Draft_17(), headers, 0) {
+
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String nombre;
+                        String msg;
+                        int priv;
+                        String det;
+
+                        try {
+                            JSONObject object = new JSONObject(message);
+                            nombre = object.getString("id");
+                            msg = object.getString("msg");
+                            priv = object.getInt("priv");
+                            det = object.getString("det");
+
+                            TextView textView = (TextView) findViewById(R.id.messages);
+                            textView.setText(textView.getText() + "\n" + nombre + " \n " + msg + " \n " + priv + " \n " + det);
+
+                        } catch (JSONException e) {
+                            TextView textView = (TextView) findViewById(R.id.messages);
+                            textView.setText(textView.getText() + "\n" + message);
+                        }
+
+
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+
+        mWebSocketClient.connect();
+
+    }
+
+
+    public void sendMessage() {
+        String user = "Pablite";
+        EditText editText = (EditText) findViewById(R.id.message);
+        CheckBox chk = (CheckBox) findViewById(R.id.chk);
+        EditText dest = (EditText) findViewById(R.id.destinatario);
+        String mensaje = editText.getText().toString();
+        int privado;
+        if (chk.isChecked()) {
+            privado = 1;
+        } else {
+            privado = 0;
+        }
+        String direccion = dest.getText().toString();
+        String miMsg = "{id:\"" + user + "\",msg:\"" + mensaje + "\",esPrivado:" + privado + ",dst:\"" + direccion + "\"}";
+
+        mWebSocketClient.send(miMsg);
+        editText.setText("");
+        dest.setText("");
+
+    }
+}
+
